aedbb4bf5e5629ae13372db800501b47736c0f28
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-02 05:58:11 +0900

    bug fix

--- include/WonderRabbitProject/key/detail.include_symbols.hxx
+++ include/WonderRabbitProject/key/detail.include_symbols.hxx
@@ -1,7 +1,7 @@
 #if defined(_WIN64) || defined(_WIN32)
   //#include "????"
 #elif defined(__APPLE__)
-  #include "/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Event.h"
+  #include "/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h"
 #elif defined(__linux)
   #include <linux/uinput.h>
 #endif
